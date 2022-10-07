2128e40cecdca50f4c85b4175ffe8f52d079aefb
August Aublet <august.aublet@gmail.com>
2022-10-07 10:04:59 +0200

    add geter to needed varibles

--- lib/data/question.dart
+++ lib/data/question.dart
@@ -2,23 +2,26 @@
 
 //Id, category, correct answer, [incorrect answer], question, difficulty
 
+import 'package:template/views/settings_view.dart';
+
 class Question {
   late String _id;
   late String _category;
   late String _correctAnswer;
   late List<String>
-      _incorrectAnswers; // Denna variabeln kommer förmodligen aldrig användas och kan tas bort
+      _incorrectAnswers; // Denna variabeln kommer förmodligen aldrig användas och kan tas bort /August
   late String _question;
   late String _difficulty;
   late List<String> _allAnswersInRandomOrder;
 
-  Question(
-      {required String id,
-      required String category,
-      required String correctAnswer,
-      required List<String> incorrectAnswers,
-      required String question,
-      required String difficulty}) {
+  Question({
+    required String id,
+    required String category,
+    required String correctAnswer,
+    required List<String> incorrectAnswers,
+    required String question,
+    required String difficulty,
+  }) {
     _id = id;
     _category = category;
     _correctAnswer = correctAnswer;
@@ -39,6 +42,14 @@ class Question {
       difficulty: json["difficulty"],
     );
   }
+  // getters
+  get id => _id;
+  get category => _category;
+  get question => _question;
+  get correctAnswer => _correctAnswer;
+  get allAnswersInRandomOrder => _allAnswersInRandomOrder;
+  get difficulty => _difficulty;
+
   List<String> scrambleAllAnswers(
     List<String> incorrectAnswers,
     String correctAnswer,

039d7ba20ba9e44b84144d3abb0703cf77a2e261
August Aublet <august.aublet@gmail.com>
2022-10-07 16:18:13 +0200

    add function increseScore, correctAnswer, incorrectAnswer

--- lib/data/player.dart
+++ lib/data/player.dart
@@ -4,16 +4,26 @@
 class Player {
   late String name;
   int _score = 0;
-  int streakCounter = 0;
+  int _streakCounter = 0;
   int _correctAnswers = 0;
-  Map playerAnswers = {};
+  Map _playerAnswers = {};
 
   Player({this.name = ""});
 
   get score => _score;
   get correctAnswers => _correctAnswers;
 
-  void addCorrectAnswer(questionId, newAnswer) {
+  void increseScore() {
+    _score += 1;
+  }
+
+  void correctAnswer(questionId, newAnswer) {
     _correctAnswers += 1;
+    _playerAnswers[questionId] = newAnswer;
+  }
+
+  void incorrectAnswer(qustionId, newAnswer) {
+    //_streakCounter = 0;
+    _playerAnswers[qustionId] = newAnswer;
   }
 }

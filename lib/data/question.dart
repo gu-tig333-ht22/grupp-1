bed9a114602933dbd4d2192b8774a22047e29900
August Aublet <august.aublet@gmail.com>
2022-10-07 09:33:50 +0200

    Variabler init och factory.

--- lib/data/question.dart
+++ lib/data/question.dart
@@ -1,4 +1,38 @@
-
 //Beskriver en fråga
 
 //Id, category, correct answer, [incorrect answer], question, difficulty
+
+class Question {
+  late String _id;
+  late String _category;
+  late String _correctAnswer;
+  late List<String> _incorrectAnswers;
+  late String _question;
+  late String _difficulty;
+
+  Question(
+      {required String id,
+      required String category,
+      required String correctAnswer,
+      required List<String> incorrectAnswers,
+      required String question,
+      required String difficulty}) {
+    _id = id;
+    _category = category;
+    _correctAnswer = correctAnswer;
+    _incorrectAnswers = incorrectAnswers;
+    _question = question;
+    _difficulty = difficulty;
+  }
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      id: json["id"],
+      category: json["category"],
+      correctAnswer: json["correctAnswer"],
+      incorrectAnswers: json["incorrectAnswers"],
+      question: json["question"],
+      difficulty: json["difficulty"],
+    );
+  }
+}

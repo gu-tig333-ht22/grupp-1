0651a61ce6dc5f68e5a7653415dcca87f8316c0e
August Aublet <august.aublet@gmail.com>
2022-10-08 10:04:21 +0200

    lagt till funktion questionAnswer som tar hand om både rätt och fel svar

--- lib/data/player.dart
+++ lib/data/player.dart
@@ -17,19 +17,36 @@ class Player {
     _score += 1;
   }
 
+  void questionAnswer(questionId, newAnswer, bool answerIsCorrect) {
+    if (answerIsCorrect) {
+      if (_streakCounter > 3) {
+        _score += 2;
+      } else {
+        _score += 1;
+      }
+      _correctAnswers += 1;
+      _streakCounter += 1;
+      _playerAnswers[questionId] = newAnswer;
+    } else if (!answerIsCorrect) {
+      _streakCounter = 0;
+      _playerAnswers[questionId] = newAnswer;
+    }
+  }
+
+  //skulle kunna göra en funktion som tar ett bool om svaret är rätt eller fel
   void correctAnswer(questionId, newAnswer) {
-    // if (_streakCounter > 3) {
-    //   _score += 2;
-    // } else {
-    //   _score += 1;
-    // }
+    if (_streakCounter > 3) {
+      _score += 2;
+    } else {
+      _score += 1;
+    }
     _correctAnswers += 1;
     _streakCounter += 1;
     _playerAnswers[questionId] = newAnswer;
   }
 
   void incorrectAnswer(qustionId, newAnswer) {
-    //_streakCounter = 0;
+    _streakCounter = 0;
     _playerAnswers[qustionId] = newAnswer;
   }
 }

489372731de5a0d24f5a1d3d5dca5fd28aed41d6
August Aublet <august.aublet@gmail.com>
2022-10-10 10:33:06 +0200

    changed to list for playerAnswer. update comments

--- lib/data/player.dart
+++ lib/data/player.dart
@@ -1,6 +1,3 @@
-// Beskriver en spelare
-// Name, points, answers
-
 /// # Player
 /// Player is the class that holds information about the player.
 ///
@@ -9,35 +6,32 @@
 /// * Player.score => current score as int.
 /// * Player.correctAnswers => number of correct answers as int.
 /// * Player.playerAnswers => Map of questionID and answer.
-/// * Player.correctAnswer(questionId, newAnswer) updates the score and streak.
-///   Also updates playerAnswer```{questionId:playerAnswer}```
-/// * Player.incorrectAnswer(questionId, newAnswer). Also updates playerAnswer
-///   ```{questionId:playerAnswer}```
+/// * Player.correctAnswer(newAnswer) updates the score and streak.
+///   Also add answer to playerAnswer.
+/// * Player.incorrectAnswer(newAnswer). Also add answer to playerAnswer.
 class Player {
   late String name;
   int _score = 0;
   int _streakCounter = 0; // if we deside to implement streakCounter
   int _longestStreak = 0; // or change score per question we also
   int _correctAnswers = 0; // need to track number of correct answers
-  Map _playerAnswers = {};
+  final List<String> _playerAnswers = [];
 
   Player({this.name = ""});
 
   /// Returns score as int.
-  get score => _score;
+  int get score => _score;
 
   /// Returns number of correctAnswer as int.
-  get correctAnswers => _correctAnswers;
+  int get correctAnswers => _correctAnswers;
 
-  /// Returns map of all the answerd questions with questionId ass key.
-  /// ```{questionId:playerAnswer}```
-  get playerAnswers => _playerAnswers;
+  /// Returns list of playerAnswers as string.
+  List<String> get playerAnswers => _playerAnswers;
 
   /// Call if player answers correctly we increse score by 1, streakCounter by 1
-  /// and correctAnswers by 1. The longest streak is also updatet. Also add the
-  /// answer(string) with questionId as key to playerAnswers map
-  /// ```{questionId:newAnswer}```.
-  void correctAnswer(questionId, newAnswer) {
+  /// and correctAnswers by 1. The longest streak is also updatet. Also add
+  /// newAnswer(string) to playerAnswers.
+  void correctAnswer({required String newAnswer}) {
     _streakCounter += 1;
     if (_streakCounter > _longestStreak) {
       _longestStreak = _streakCounter;
@@ -48,18 +42,18 @@ class Player {
       _score += 1;
     }
     _correctAnswers += 1;
-    _playerAnswers[questionId] = newAnswer;
+    _playerAnswers.add(newAnswer);
   }
 
   /// Call if player answers incorectly, we update the longest streak if
-  /// streakCounter is longer and the set streakCounter to 0. Also add the
-  /// answer(string) with questionId as key to playerAnswers map
-  /// ```{questionId:newAnswer}```.
-  void incorrectAnswer(qustionId, newAnswer) {
+  /// streakCounter is longer and the set streakCounter to 0. Also add
+  /// newAnswer(string) to playerAnswers. Default value ="No answer" if player
+  /// did not answer.
+  void incorrectAnswer({String newAnswer = "No answer"}) {
     if (_streakCounter > _longestStreak) {
       _longestStreak = _streakCounter;
     }
     _streakCounter = 0;
-    _playerAnswers[qustionId] = newAnswer;
+    _playerAnswers.add(newAnswer);
   }
 }

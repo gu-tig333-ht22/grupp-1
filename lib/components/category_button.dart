f6c5af461852632cfdee54b54c865f9e3ac0b7b6
August Aublet <august.aublet@gmail.com>
2022-10-11 22:13:35 +0200

    remoced some old code add inkwell and dynamic color

--- lib/components/category_button.dart
+++ lib/components/category_button.dart
@@ -1,39 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:template/theme/theme.dart';
 
-Container categorySelectionBox(
-    {icon, required Color color, required double size}) {
-  var themeIcons =
-      ThemeIcons(); // Är det så det är tänkt att göra se rad 17 ich.
-  return Container(
-    width: size,
-    height: size,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(15),
-      gradient: LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-          themeIcons.lightenColor(color, 40),
-          color,
-          themeIcons.darkenColor(color, 60)
-        ],
-        stops: const [
-          0,
-          0.2,
-          0.9,
-        ],
+InkWell categorySelectionBox(category) {
+  List tempValdaKategorier = [
+    'Art & Literature',
+    'General Knowledge',
+    'Geography',
+  ];
+  var themeIcons = ThemeIcons(); // Är det så det är tänkt att
+  // göra se rad 17 ich.
+  Color categoryColor = category.color;
+
+  if (!tempValdaKategorier.contains(category.name)) {
+    categoryColor =
+        Themes.colors.greyLight; // skall vara någon form av filter i stället
+  }
+  return InkWell(
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            themeIcons.lightenColor(categoryColor, 40),
+            categoryColor,
+            themeIcons.darkenColor(categoryColor, 60)
+          ],
+          stops: const [
+            0,
+            0.2,
+            0.9,
+          ],
+        ),
+      ),
+      child: FittedBox(
+        child: Padding(
+          padding: const EdgeInsets.all(4),
+          child: Center(
+              child: Icon(
+            category.icon,
+            color: Themes.colors.yellowLight, // vad skall det vara för färg?
+          )),
+        ),
       ),
     ),
-    child: FittedBox(
-      child: Padding(
-        padding: const EdgeInsets.all(4),
-        child: Center(
-            child: Icon(
-          icon,
-          color: Themes.colors.yellowLight, // vad skall det vara för färg?
-        )),
-      ),
-    ),
+    onTap: () {
+      // update provider and redraw
+    },
   );
 }

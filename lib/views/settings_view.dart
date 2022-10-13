62a4594726b8084b0c53e05ae785dd524d27e58f
August Aublet <august.aublet@gmail.com>
2022-10-13 11:02:34 +0200

    Flyttat category button till settings_view

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_octicons/flutter_octicons.dart';
 import 'package:provider/provider.dart';
 import 'package:template/theme/theme.dart';
-import 'package:template/components/category_button.dart';
-
 import '../data/game_session.dart';
 
 class SettingsView extends StatelessWidget {
@@ -74,6 +72,66 @@ class CategoryRow extends StatelessWidget {
               .toList(),
         ),
       ],
+    );
+  }
+}
+
+/// # Categorybutton
+/// Builds a button for categories. Updates the categories selected in settings
+class CategoryButton extends StatelessWidget {
+  var category;
+  CategoryButton(this.category, {super.key});
+
+  // ignore: empty_constructor_bodies
+  @override
+  Widget build(BuildContext context) {
+    List tempValdaKategorier =
+        Provider.of<GameSession>(context, listen: true).chosenCategories;
+
+    Color categoryColor = category.color;
+    Color iconColor = Themes.colors.whiteBackground;
+    double opacity = 1;
+    if (!tempValdaKategorier.contains(category.name)) {
+      categoryColor = Themes.icons.lightenColor(categoryColor, 40);
+      opacity = 0.40;
+    }
+    return InkWell(
+      child: Opacity(
+        opacity: opacity,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Themes.icons.lightenColor(categoryColor, 40),
+                categoryColor,
+                Themes.icons.darkenColor(categoryColor, 60)
+              ],
+              stops: const [
+                0,
+                0.2,
+                0.9,
+              ],
+            ),
+          ),
+          child: FittedBox(
+            child: Padding(
+              padding: const EdgeInsets.all(4),
+              child: Center(
+                  child: Icon(
+                category.icon,
+                color: iconColor, // vad skall det vara för färg?
+              )),
+            ),
+          ),
+        ),
+      ),
+      onTap: () {
+        Provider.of<GameSession>(context, listen: false)
+            .updateCategory(category.name);
+      },
     );
   }
 }

9852d1fb4917e1d007b6f8907a99370ebe4b24a9
August Aublet <august.aublet@gmail.com>
2022-10-14 11:22:44 +0200

    extra

--- lib/views/question_view.dart
+++ lib/views/question_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:template/components/card.dart';
-import 'package:template/data'
+import 'package:template/data/game_session.dart';
 
 import 'dart:math' as math;
 

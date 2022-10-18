af9e68298b64f8da0e9712cd1eff2be5dbc58aa5
August Aublet <august.aublet@gmail.com>
2022-10-18 17:13:43 +0200

    Fixat en timer:)

--- lib/views/loading_screen.dart
+++ lib/views/loading_screen.dart
@@ -1,42 +1,51 @@
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:template/data/game_session.dart';
 import 'package:template/theme/theme.dart';
 import 'package:template/views/question_view.dart';
+import 'package:timer_count_down/timer_controller.dart';
+import 'package:timer_count_down/timer_count_down.dart';
 
-class LoadingView extends StatefulWidget {
-  String countDown = "";
-
-  @override
-  State<LoadingView> createState() => _LoadingViewState();
-}
-
-class _LoadingViewState extends State<LoadingView> {
-  @override
-  void initState() async {
-    super.initState();
-    _navigateTo();
-  }
-
-  _navigateTo() async {
-    await Provider.of<GameSession>(context, listen: false).startGame();
-    Navigator.push(
-        context,
-        PageRouteBuilder(
-            pageBuilder: (context, _, __) => QuestionView(),
-            transitionDuration: Duration.zero,
-            reverseTransitionDuration: Duration.zero));
-  }
-
+class LoadingView extends StatelessWidget {
+  final CountdownController _controller =
+      new CountdownController(autoStart: false);
   @override
   Widget build(BuildContext context) {
+    void fetching() async {
+      await Provider.of<GameSession>(context, listen: false).startGame();
+      _controller.start();
+    }
+
+    fetching();
+
     return ScaffoldWithBackground(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("Get Ready!", style: Themes.textStyle.headline1),
-          Text(widget.countDown)
-        ],
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Get Ready!",
+              style: Themes.textStyle.headline1,
+            ),
+            Countdown(
+              controller: _controller,
+              seconds: 3,
+              build: (_, double time) => Text(
+                time.toString(),
+                style: Themes.textStyle.headline1,
+              ),
+              onFinished: () {
+                Navigator.pushReplacement(
+                    context,
+                    PageRouteBuilder(
+                        pageBuilder: (context, _, __) => QuestionView(),
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }

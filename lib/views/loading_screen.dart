0e7a78f0eef4fba291e759d1a6b7975b75e17b23
August Aublet <august.aublet@gmail.com>
2022-10-19 09:46:20 +0200

    Tagit bort 0an och ersat med Go!

--- lib/views/loading_screen.dart
+++ lib/views/loading_screen.dart
@@ -1,4 +1,3 @@
-import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:template/data/game_session.dart';
@@ -8,12 +7,15 @@ import 'package:timer_count_down/timer_controller.dart';
 import 'package:timer_count_down/timer_count_down.dart';
 
 class LoadingView extends StatelessWidget {
-  final CountdownController _controller =
-      new CountdownController(autoStart: false);
+  final CountdownController _controller = CountdownController(autoStart: false);
+
+  LoadingView({super.key});
   @override
   Widget build(BuildContext context) {
+    bool getReady = true;
     void fetching() async {
       await Provider.of<GameSession>(context, listen: false).startGame();
+      getReady = false;
       _controller.start();
     }
 
@@ -24,17 +26,27 @@ class LoadingView extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "Get Ready!",
-              style: Themes.textStyle.headline1,
-            ),
+            //const SizedBox(height: 20),
+            Themes.textStyle.headlineGradient(text: 'Quizter', fontSize: 44),
+            Themes.textStyle.headlineGradient(text: 'Pettersson', fontSize: 44),
+            const SizedBox(height: 200),
             Countdown(
               controller: _controller,
-              seconds: 3,
-              build: (_, double time) => Text(
-                time.toString(),
-                style: Themes.textStyle.headline1,
-              ),
+              seconds: 4, // Behöver vara 4a då Get ready inte hinner försvinna.
+              build: (_, double time) => getReady
+                  ? Text(
+                      "Get Ready!",
+                      style: Themes.textStyle.headline1,
+                    )
+                  : time <= 0
+                      ? Text(
+                          "Go!",
+                          style: Themes.textStyle.headline1,
+                        )
+                      : Text(
+                          time.toString(),
+                          style: Themes.textStyle.headline1,
+                        ),
               onFinished: () {
                 Navigator.pushReplacement(
                     context,
